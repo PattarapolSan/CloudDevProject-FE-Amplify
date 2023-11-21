97fdc8003a190e1ec0b61e3ecbe22eae53ed88e7
PattarapolSangprakai <6322770403@g.siit.tu.ac.th>
2023-11-22 05:22:32 +0700

    Test cookie2

--- cloud-dev-project/src/app/api/auth/route.ts
+++ cloud-dev-project/src/app/api/auth/route.ts
@@ -1,32 +1,15 @@
-// import { NextRequest } from "next/server";
-// export async function GET(request: NextRequest) {
-//     const authToken = request.cookies.get("authToken");
-//     const res = {
-//         statusCode: 200,
-
-//         headers: {
-//             'Access-Control-Allow-Origin': '*',
-//             'Access-Control-Allow-Headers': '*',
-//         },
-//         body: JSON.stringify({ ...authToken })
-//     }
-//     return new Response(JSON.stringify({ ...authToken }));
-// }
-
-import { NextRequest, NextResponse } from 'next/server';
-
+import { NextRequest, NextResponse } from "next/server";
 export async function GET(request: NextRequest) {
-  // Extract the value of the 'authToken' cookie
-  const authToken = request.cookies.get('authToken');
+    const authToken = request.cookies.get("authToken");
+    const res = {
+        statusCode: 200,
 
-  // Your logic here...
-
-  // Prepare the response
-  const res = NextResponse.json({ authToken });
-
-  // Set CORS headers
-  res.headers.set('Access-Control-Allow-Origin', '*');
-  res.headers.set('Access-Control-Allow-Headers', '*');
-
-  return res;
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': '*',
+        },
+        body:NextResponse.json({ ...authToken })
+    }
+    return NextResponse.json({ ...authToken });
 }
+
